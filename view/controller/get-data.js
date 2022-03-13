b6b7a8b8886c81a7cb4dee38b4787eb023a1f62b
luischica25 <lachica@uninorte.edu.co>
2022-03-12 19:54:12 -0500

    aggmarkers

--- view/controller/get-data.js
+++ view/controller/get-data.js
@@ -1,18 +1,27 @@
 async function getData(){
-    data = await fetch('/data')
-    return data = await data.json()
+    var data = await fetch('/data')
+    if(!data.ok) throw data.status
+    data = data.json()
+    return data
 }
+var latitud = 0 ,longitud = 0
 setInterval(async ()=>{
-    await getData().then((data)=>{
-        data = data.response
-        fecha = new Date(data.fecha)
-        month = parseInt(fecha.getMonth()) + 1
-        var month
-        fechaFormateada = fecha.getDate()+'/'+month+'/'+(fecha.getYear()).toString().substring(1,fecha.getYear().length)+' '+ data.hora
-        console.log()
-        document.getElementById('lat').innerHTML = data.latitud
-        document.getElementById('long').innerHTML = data.longitud
-        document.getElementById('fecha').innerHTML = fechaFormateada
-     })
+    data = await getData();
+    console.log(data.response)
+    response = setDataIntDoc(data.response)
+    console.log(response)
+    map.setView([response[0], response[1]], 13)
+    L.marker([response[0], response[1]]).addTo(map)
+},2000)
 
-},5000)
+function setDataIntDoc(data){
+    var fecha = new Date(data.fecha)
+    var month = parseInt(fecha.getMonth()) + 1
+    var fechaFormateada = fecha.getDate()+'/'+month+'/'+(fecha.getYear()).toString().substring(1,fecha.getYear().length)+' '+data.hora
+
+    document.getElementById('lat').innerHTML = data.latitud
+    document.getElementById('long').innerHTML = data.longitud
+    document.getElementById('fecha').innerHTML = fechaFormateada
+
+    return [data.latitud,data.longitud]
+}
