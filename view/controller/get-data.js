4df8612a2900022aaf37f20834e9e21d132d126e
luischica25 <lachica@uninorte.edu.co>
2022-03-30 14:37:01 -0500

    ac

--- view/controller/get-data.js
+++ view/controller/get-data.js
@@ -10,8 +10,15 @@ setInterval(async ()=>{
     data = await getData();
     response = setDataIntDoc(data.response)
     let markerArray = new Array()
+    if(marker!==undefined){
+        map.removeLayer(marker)
+    }
     actualMarket = L.marker([response[0],response[1]]).addTo(map)
-    
+    if(polyline!==undefined){
+        map.removeLayer(polyline)
+    }
+    var actualpolyline = L.polyline(markerArray).addTo(map);
+
 },4000)
 
 function setDataIntDoc(data){
